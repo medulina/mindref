f0ae79547a39f70c96d1d9e43c8086256f64d7a5
akeshavan <akeshavan@ucla.edu>
2017-09-06 22:13:21 +0000

    Fix test score bug

--- dockereve-master/eve-app/app.py
+++ dockereve-master/eve-app/app.py
@@ -325,29 +325,34 @@ def get_cfx_masks(truth, attempt):
     return cm
 
 def post_post_mask(request, payload):
+    if request.args['mode'] == 'try':
+        resp = json.loads(payload.response[0].decode("utf-8"))
+        #raise Exception(str(payload.headers['Content-Length']) + '  ' + str(len(payload.response[0])))
+        mask_id = resp['_id']
+        masks = app.data.driver.db['mask']
+        mask = masks.find_one({'_id': ObjectId(mask_id)})
+        truth = masks.find_one({'image_id': ObjectId(mask['image_id']), 'mode': 'truth'})
+        cm = get_cfx_masks(truth['pic'], mask['pic'])
+        resp['score'] = mask['score']
+        # TODO: Make this code work for multiclass
+        try: 
+            resp['tp'] = cm[1][1]
+        except KeyError:
+            resp['tp'] = {}
+        try:
+            resp['fp'] = cm[0][1]
+        except KeyError:
+            resp['fp'] = {}
+        try:
+            resp['fn'] = cm[1][0]
+        except KeyError:
+            resp['fn'] = {}
+        payload.response[0] = json.dumps(resp).encode()
+        payload.headers['Content-Length'] = len(payload.response[0])
+
+def post_get_maskagg(request, payload):
     resp = json.loads(payload.response[0].decode("utf-8"))
-    #raise Exception(str(payload.headers['Content-Length']) + '  ' + str(len(payload.response[0])))
-    mask_id = resp['_id']
-    masks = app.data.driver.db['mask']
-    mask = masks.find_one({'_id': ObjectId(mask_id)})
-    truth = masks.find_one({'image_id': ObjectId(mask['image_id']), 'mode': 'truth'})
-    cm = get_cfx_masks(truth['pic'], mask['pic'])
-    resp['score'] = mask['score']
-    # TODO: Make this code work for multiclass
-    try: 
-        resp['tp'] = cm[1][1]
-    except KeyError:
-        resp['tp'] = {}
-    try:
-        resp['fp'] = cm[0][1]
-    except KeyError:
-        resp['fp'] = {}
-    try:
-        resp['fn'] = cm[1][0]
-    except KeyError:
-        resp['fn'] = {}
-    payload.response[0] = json.dumps(resp).encode()
-    payload.headers['Content-Length'] = len(payload.response[0])
+
 
 app.on_insert_mask += on_insert_mask
 app.on_pre_GET_image += pre_image_get_callback
